8cdb04fd48035e9e9ffab404aae4eef632e5b390
jkow1 <jkowalczuk1@sii.pl>
2022-10-18 18:33:29 +0200

    Refactor

--- src/main/java/weather/WeatherResponse.java
+++ src/main/java/weather/WeatherResponse.java
@@ -1,4 +1,17 @@
 package weather;
 
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.specification.ResponseSpecification;
+import static org.hamcrest.Matchers.is;
+
 public class WeatherResponse {
+
+    public static ResponseSpecification getResponseSpec(String name, String country, int id) {
+        return new ResponseSpecBuilder()
+                .expectBody("name",is(name))
+                .expectBody("sys.country",is(country))
+                .expectBody("id", is(id))
+                .build()
+                .statusCode(200);
+    }
 }
--- src/test/java/weather/WeatherTest.java
+++ src/test/java/weather/WeatherTest.java
@@ -6,24 +6,25 @@ import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static io.restassured.RestAssured.given;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class WeatherTest extends TestBase {
 
-    @DisplayName("Get should respond with weather info for city ID")
-    @ParameterizedTest(name = "{index}. Test for city with id {0}")
-    @ValueSource(ints = {2643743,2640729,3099434})
+    @DisplayName("Should get response with weather info for city ID")
+    @ParameterizedTest(name = "{index}. Test for city {0} with id {2}")
+    @CsvFileSource(resources = "/cities.csv")
     @Tag("weather")
     @Tag("regression")
-    public void shouldGetWeatherInfoByCityIDTest(int id) {
+    public void shouldGetWeatherInfoByCityIDTest(String city, String country, int id) {
         given().
                 spec(WeatherRequest.getWeatherInfoByCityID(id)).
         when().
-                get();
-
+                get().
+        then().
+                spec(WeatherResponse.getResponseSpec(city, country, id));
     }
 
 
